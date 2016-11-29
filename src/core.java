574a04afeca13aa9b6cc8e627347c5f7af294352
Josh Brown <brownbear1002@gmail.com>
2016-11-29 16:35:58 -0500

    Update core.java v0.2
    Imported required library's
    Created package - " org.usfirst.frc.team191.core ".

--- src/core.java
+++ src/core.java
@@ -1,3 +1,27 @@
-public class core{
-  println("Hello World");
+/*
+* 
+* Author - Josh Brown
+* Date - Nov 29
+* Time 4:32
+*/
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
+//Creating the package name maunualy.
+package  org.usfirst.frc.team191.core;
+  
+/*
+* This should call all commands and functions
+* The main part is for initializing all code
+* and making it available to run actions
+*/
+public class core extends IterativeRobot{
+  println("Hello World);
+}
+
+public static class defaultAction{
+ println("this is where you would put your default action"); 
 }
