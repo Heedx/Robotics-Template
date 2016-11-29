47e63d4269aea8fdc77f810b63eec1debfe8b944
Josh Brown <brownbear1002@gmail.com>
2016-11-29 16:48:46 -0500

    Update core.java
    Added function coreInit which starts our desired action
    added variables:
         Drive myRobot
         Joystick stick
         autoloopCounter
    
    added detailed description in comments

--- src/core.java
+++ src/core.java
@@ -1,5 +1,10 @@
 /*
 * 
+* File Name - core.java
+* Description -
+*   core.java initializes all commands and gives each one a purpose and function.
+*   this also runs the code. so what ever we want it to do we would initialize it here.
+* 
 * Author - Josh Brown
 * Date - Nov 29
 * Time 4:32
@@ -19,9 +24,16 @@ package  org.usfirst.frc.team191.core;
 * and making it available to run actions
 */
 public class core extends IterativeRobot{
-  println("Hello World);
-}
+  //Defines variables as memebers of the core (filename)
+  Drive myRobot;
+  Joystick stick;
+  int autoloopCounter;
 
-public static class defaultAction{
- println("this is where you would put your default action"); 
+  //Ininitalizes variables in the coreInit method. This function is called when it is initializing
+  public void coreInit() {
+    Drive = new RobotDrive(0,1); 
+    // This variable assigns our joystick to USB prot one. (One on the farthes right.)
+    // If having issues try keeping the Joystick plugged in to a port then change the number +1 to what ever number of usb ports.
+    stick =  new Joystick(1);
+  }
 }
